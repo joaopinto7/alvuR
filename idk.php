58acf53926ebc05d44dad0df9b005aa9bfc3e072
joaopinto7 <akawe15@gmail.com>
2015-03-19 20:06:46 -0300

    Update idk.php

--- idk.php
+++ idk.php
@@ -3,8 +3,11 @@
 //Verifica se um argumento foi usado 
 if (isset($argv[1])) {
 	echo $argv[1];
-} else {
-	echo "Por favor use um argumento";
+} else if(isset($argv[1]) and $argv[1] == "livre"){echo "Por favor use um argumento";
+	
+}else {
+	
+	echo "Tente novamente!";
 }
 
 
